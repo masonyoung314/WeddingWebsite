9cd6394acf989bbc5c1f90da6cd9b16860486fc6
Mason Young <masonyou@umich.edu>
2026-05-19 19:46:00 +0100

    Navbar work and start on game

--- MasonAndSofia/src/components/NavBar.tsx
+++ MasonAndSofia/src/components/NavBar.tsx
@@ -8,8 +8,8 @@ const NavBar = (props: Props) => {
   return (
     <div className={styles.navbar}>
         <div className={styles.navLinks}>
-            <Link to="/" className={styles.navIcon}>Mason and Sofia</Link>
             <Link to="/game" className={styles.navIcon}>Game</Link>
+            <Link to="/" className={styles.navIcon} id={styles.one}>Mason and Sofia</Link>
             <Link to="/registry" className={styles.navIcon}>Registry</Link>
         </div>
     </div>
--- MasonAndSofia/src/pages/Game.tsx
+++ MasonAndSofia/src/pages/Game.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import phone from "../assets/Iphone.png";
+import styles from "../styles/Game.module.css";
+import good from "../assets/Good.png";
+import bad from "../assets/Bad.png";
 
 type Props = {};
 
 function Game({}: Props) {
   return (
-    <div>
-      <h1>Game</h1>
-    </div>
+    <>
+      <div className={styles.phoneContainer}>
+        <img src={phone} alt="Iphone Outline" className={styles.img}/>
+        <input type="image" src={good} name="saveForm" className={styles.goodBtn} />
+        <input type="image" src={bad} name="saveForm" className={styles.badBtn} />
+        <img src={choice} alt="Image of wedding dress"></img>
+      </div>
+    </>
+    
   );
 };
 
