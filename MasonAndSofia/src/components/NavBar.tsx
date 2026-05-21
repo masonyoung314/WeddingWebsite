15c195f21f0064224bb77be5955c265f8910b06d
Mason Young <masonyou@umich.edu>
2026-05-21 15:11:20 +0100

    Select images until one left functionality

--- MasonAndSofia/src/components/NavBar.tsx
+++ MasonAndSofia/src/components/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = (props: Props) => {
     <div className={styles.navbar}>
         <div className={styles.navLinks}>
             <Link to="/game" className={styles.navIcon}>Game</Link>
-            <Link to="/" className={styles.navIcon} id={styles.one}>Mason and Sofia</Link>
+            <Link to="/" className={`${styles.navIcon} ${styles.largeTitle}`} style={{fontSize: '3rem'}}>Mason and Sofia</Link>
             <Link to="/registry" className={styles.navIcon}>Registry</Link>
         </div>
     </div>
--- MasonAndSofia/src/pages/Game.tsx
+++ MasonAndSofia/src/pages/Game.tsx
@@ -1,22 +1,94 @@
 import React from "react";
+import { useState } from "react";
 import phone from "../assets/Iphone.png";
 import styles from "../styles/Game.module.css";
 import good from "../assets/Good.png";
 import bad from "../assets/Bad.png";
+import one from "../assets/coolThingOne.png";
+import two from "../assets/coolThingTwo.jpeg";
+import three from "../assets/coolThingThree.jpg";
+import four from "../assets/coolThingFour.webp";
+import five from "../assets/coolThingFive.jpg";
 
 type Props = {};
 
+// let goodDresses: string[] = [];
+
 function Game({}: Props) {
+
+  const [currDress, setCurrDress] = useState(one);
+  const [winner, setWinner] = useState("");
+  const [dresses, setDresses] = useState([one, two, three, four, five]);
+
+  function chooseImg(arr: string[]) {
+    const numDresses: number = arr.length;
+
+    let idx: number = Math.floor(Math.random() * numDresses);
+    let choice: string = arr[idx];
+
+    while (choice == currDress && arr.length > 1) {
+      idx = Math.floor(Math.random() * numDresses);
+      choice = arr[idx];
+    }
+
+    return choice;
+  };
+
+  const goodClick = () => {
+
+    if (dresses.length === 1) {
+      setWinner(dresses[0]);
+      setCurrDress(dresses[0]);
+      return;
+    }
+
+    let newCurrDress = chooseImg(dresses);
+    setCurrDress(newCurrDress);
+
+  };
+
+  const badClick = () => {
+    const updatedDresses = dresses.filter(d => d !== currDress);
+    setDresses(updatedDresses);
+
+    if (updatedDresses.length === 1) {
+      setWinner(updatedDresses[0]);
+      setCurrDress(updatedDresses[0]);
+      return;
+    }
+
+    let newCurrDress = chooseImg(updatedDresses);
+    setCurrDress(newCurrDress);
+
+  };
+
   return (
     <>
-      <div className={styles.phoneContainer}>
-        <img src={phone} alt="Iphone Outline" className={styles.img}/>
-        <input type="image" src={good} name="saveForm" className={styles.goodBtn} />
-        <input type="image" src={bad} name="saveForm" className={styles.badBtn} />
-        <img src={choice} alt="Image of wedding dress"></img>
+      <div className={styles.gameWrapper}>
+        <div className={styles.phoneContainer}>
+          <img src={phone} alt="Iphone Outline" className={styles.img}/>
+          {(winner == "") && <input 
+          type="image"  
+          src={good} 
+          name="saveForm"
+          onClick={goodClick} 
+          className={styles.goodBtn} />}
+          {(winner == "") && <input 
+          type="image" 
+          src={bad} 
+          name="saveForm" 
+          onClick={badClick}
+          className={styles.badBtn} />}
+          {winner == "" ? 
+          (<img src={currDress} alt="Image of wedding dress" className={styles.dress}/> 
+          ) : (<img src={winner} alt="Image of winning dress" className={styles.dress} />
+
+          )}
+          {winner !== "" && <h1 className={styles.winnerMessage}>Your final guess is...</h1>}
+        </div>
       </div>
+      
     </>
-    
   );
 };
 
