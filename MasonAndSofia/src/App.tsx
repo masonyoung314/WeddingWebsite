21d3fcba20843887c35ec4c031dec405b999585b
Mason Young <masonyou@umich.edu>
2026-05-24 22:50:33 +0100

    Added two more pages

--- MasonAndSofia/src/App.tsx
+++ MasonAndSofia/src/App.tsx
@@ -4,6 +4,8 @@ import NavBar from './components/navbar';
 import Home from './pages/Home';
 import Game from './pages/Game';
 import Registry from './pages/Registry';
+import Information from './pages/Information';
+import Attendance from './pages/Attendance';
 
 function App() {
 
@@ -15,6 +17,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/game" element={<Game />} />
           <Route path="/registry" element={<Registry />} />
+          <Route path="/info" element={<Information />} />
+          <Route path ="/attendance" element={<Attendance />} />
         </Routes>
       </main>
     </>
--- MasonAndSofia/src/components/NavBar.tsx
+++ MasonAndSofia/src/components/NavBar.tsx
@@ -8,9 +8,13 @@ const NavBar = (props: Props) => {
   return (
     <div className={styles.navbar}>
         <div className={styles.navLinks}>
-            <Link to="/game" className={styles.navIcon}>Game</Link>
-            <Link to="/" className={`${styles.navIcon} ${styles.largeTitle}`} style={{fontSize: '3rem'}}>Mason and Sofia</Link>
-            <Link to="/registry" className={styles.navIcon}>Registry</Link>
+          <Link to="/info" className={styles.navIcon}>Info</Link>
+          <Link to="/game" className={styles.navIcon}>Game</Link>
+
+          <Link to="/" className={`${styles.navIcon} ${styles.largeTitle}`} style={{fontSize: '3rem'}}>Mason and Sofia</Link>
+          
+          <Link to="/registry" className={styles.navIcon}>Registry</Link>
+          <Link to="/attendance" className={styles.navIcon}>Attendance</Link>
         </div>
     </div>
   )
