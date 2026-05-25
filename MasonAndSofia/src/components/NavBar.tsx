ac88ef6fb24628da915a898192faee5fc0e00a85
Mason Young <masonyou@umich.edu>
2026-05-25 12:42:04 +0100

    Cleaned up

--- MasonAndSofia/src/components/NavBar.tsx
+++ MasonAndSofia/src/components/NavBar.tsx
@@ -7,7 +7,6 @@ type Props = {}
 const NavBar = (props: Props) => {
   return (
     <div className={styles.navbar}>
-        {/* <div className={styles.navLinks}> */}
           <div className={styles.smallLinks}>
             <Link to="/info" className={styles.navIcon}>Information</Link>
             <Link to="/game" className={styles.navIcon}>Game</Link>
@@ -19,7 +18,6 @@ const NavBar = (props: Props) => {
             <Link to="/registry" className={styles.navIcon}>Registry</Link>
             <Link to="/attendance" className={styles.navIcon}>Attendance</Link>
           </div>
-        {/* </div> */}
     </div>
   )
 }
