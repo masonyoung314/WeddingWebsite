94f10d781dcb6dc6b3a272df519b1257cab3586e
Mason Young <masonyou@umich.edu>
2026-05-25 12:41:10 +0100

    Fixed centering issue with navbar

--- MasonAndSofia/src/components/NavBar.tsx
+++ MasonAndSofia/src/components/NavBar.tsx
@@ -7,15 +7,19 @@ type Props = {}
 const NavBar = (props: Props) => {
   return (
     <div className={styles.navbar}>
-        <div className={styles.navLinks}>
-          <Link to="/info" className={styles.navIcon}>Info</Link>
-          <Link to="/game" className={styles.navIcon}>Game</Link>
-
+        {/* <div className={styles.navLinks}> */}
+          <div className={styles.smallLinks}>
+            <Link to="/info" className={styles.navIcon}>Information</Link>
+            <Link to="/game" className={styles.navIcon}>Game</Link>
+          </div>
+        
           <Link to="/" className={`${styles.navIcon} ${styles.largeTitle}`} style={{fontSize: '3rem'}}>Mason and Sofia</Link>
-          
-          <Link to="/registry" className={styles.navIcon}>Registry</Link>
-          <Link to="/attendance" className={styles.navIcon}>Attendance</Link>
-        </div>
+
+          <div className={styles.smallLinks}>
+            <Link to="/registry" className={styles.navIcon}>Registry</Link>
+            <Link to="/attendance" className={styles.navIcon}>Attendance</Link>
+          </div>
+        {/* </div> */}
     </div>
   )
 }
