af4f3a86ba7c20256a3ffca4cb212b85c8f37e70
Mason Young <masonyou@umich.edu>
2026-05-28 17:08:26 +0100

    Set up database with guests name and attendance list functional

--- MasonAndSofia/frontend/src/pages/Attendance.tsx
+++ MasonAndSofia/frontend/src/pages/Attendance.tsx
@@ -1,18 +1,84 @@
-import React from 'react'
 import { useState, useEffect } from 'react';
+import { collection, query, orderBy, onSnapshot, serverTimestamp } from 'firebase/firestore';
+import type { DocumentData } from 'firebase/firestore';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase.ts';
+import styles from "../styles/Attendance.module.css";
 
 
 type Guest = {
-  id: number;
+  id: string;
   name: string;
   attending: boolean;
 };
 
+const handleBoxChange = async (id: string, isChecked: boolean) => {
+  try {
+    const guest = doc(db, 'attendees', id);
+    await updateDoc(guest, {
+      attending: isChecked,
+      lastUpdated: serverTimestamp()
+    });
+    console.log(`Updated ${id}'s attending status to: ${isChecked}`);
+  }
+  catch (err) {
+    console.error("Error in updating guest attendance", err);
+    return (<p>Error in updating attendance status</p>);
+  }
+};
+
+
 const Attendance = () => {
   const [guests, setGuests] = useState<Guest[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
+  useEffect(() => {
+    const q = query(collection(db, 'attendees'), orderBy('name'));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const guestData: Guest[] = [];
+      snapshot.forEach((doc) => {
+        const data = doc.data() as DocumentData;
+        guestData.push({
+          id: doc.id,
+          name: data.name,
+          attending: data.attending
+        });
+      });
+      setGuests(guestData);
+      setLoading(false);
+    }, (err) => {
+      console.error("Error fetching data from database: ", err);
+      setError("Failed to load guest data.");
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return (<p>Page loading...</p>)
+  }
+  if (error) {
+    return <p style={{color:'red'}}>Error: {error}</p>
+  }
   return (
-    <div>Attendance</div>
+    <>
+    <div className={styles.attendanceScreen}>
+      <h1>Guest List</h1>
+      <ul style={{listStyle: 'none'}}>
+        {guests.map((guest) => (
+          <li key={guest.id}>
+            <label>
+              <input type='checkbox' checked={guest.attending} onChange={(e) => handleBoxChange(guest.id, e.target.checked)} />
+              {guest.name}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+    </>
   )
 }
 
