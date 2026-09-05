aef26ca249fe8b5ff5ab231b12d775225fb501af
Mason Young <masonyou@umich.edu>
2026-09-05 14:05:16 -0400

    Fixed navbar button

--- MasonAndSofia/frontend/src/components/NavBar.tsx
+++ MasonAndSofia/frontend/src/components/NavBar.tsx
@@ -1,36 +1,27 @@
 import styles from "../styles/NavBar.module.css";
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { AnimatePresence, motion } from 'motion/react';
 import btn from "../assets/burgerBtn.png";
 import logo from "../assets/mason_and_sofia_logo.svg";
 
 type Props = {}
 
-let rotation: number = 0;
-
-const spinBtn = () => {
-  const btn: HTMLElement | null = document.getElementById("burgerBtn")!;
-  rotation += 360;
-  btn.style.transform = `rotate(${rotation}deg)`;
-}
+// let rotation: number = 0;
 
 function NavBar({}: Props) {
-  const [isBtnSelected, setBtnSelected] = useState<boolean>(true);
-  useEffect(() => {
-    setBtnSelected(false);
-  }, []);
+  const [isBtnSelected, setBtnSelected] = useState<boolean>(false);
 
+  const [rotation, setRotation] = useState<number>(0);
+
+  const spinBtn = () => {
+    const btn: HTMLElement | null = document.getElementById("burgerBtn")!;
+    setRotation(rotation + 360);
+    btn.style.transform = `rotate(${rotation}deg)`;
+  }
   const handleBurgerClick = () => {
-
-    if (isBtnSelected) {
-      setBtnSelected(false);
-      spinBtn();
-    }
-    else {
-      setBtnSelected(true);
-      spinBtn();
-    }
+    setBtnSelected(!isBtnSelected);
+    spinBtn();
   }
 
   return (
@@ -58,9 +49,6 @@ function NavBar({}: Props) {
                   <Link to="/attendance" className={styles.navIcon}>Attendance</Link>
                 </motion.div>
               </AnimatePresence>
-            
-            
-            
       </div>
       <div className={styles.navButton}>
         <input 
